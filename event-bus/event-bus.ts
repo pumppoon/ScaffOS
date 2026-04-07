a7ff7ba23a0f871fa686157ed373ac2d3b744c80
forge <forge@scaffos.dev>
2026-04-07 22:50:53 +0000

    feature(event-bus): Add a new useful feature or endpoint. Think about what's mis

--- event-bus/event-bus.ts
+++ event-bus/event-bus.ts
@@ -1,6 +1,5 @@
-import { RedisClient } from 'redis';
-
 export interface EventBus {
     publish(event: string, data: any): Promise<void>;
     subscribe(event: string, listener: (data: any) => void): Promise<void>;
+    getLatestData(event: string): Promise<any | null>;
 }
